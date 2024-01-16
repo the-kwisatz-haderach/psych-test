50278e469e004028c7f6870eef739decd1b4e196
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2024-01-16 18:46:41 +0100

    install icon package

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -4,7 +4,8 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 const config = {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte-icons/**/*.{html,js,svelte,ts}'
 	],
 
 	plugins: [require('flowbite/plugin')],
