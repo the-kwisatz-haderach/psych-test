5df9d2064ee0ba8ae06f687fde1cbafc46790dd3
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2024-02-02 11:41:25 +0100

    init terraform with cloud bucket state

---
+++ terraform/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  backend "gcs" {
+    bucket = "a22fea2f9857fc71-bucket-tfstate"
+    prefix    = "terraform/state"
+  }
+  
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.51"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "google" {
+  project = "psych-411610"
+  region  = "europe-west1"
+}
+
+resource "google_service_account" "terraform" {
+  account_id   = "tf-admin"
+  display_name = "A service account allowing terraform to manage cloud resources"
+}
+
+resource "google_project_iam_member" "default" {
+  project    = "psych-411610"
+  role       = "roles/admin"
+  member     = google_service_account.terraform.member
+  depends_on = [google_service_account.terraform]
+}
+
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "tf_state" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  force_destroy = false
+  location      = "EU"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = false
+  }
+  depends_on = [
+    google_project_iam_member.default
+  ]
+}
